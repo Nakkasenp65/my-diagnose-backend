03b61e6a086e57a33699c8e594d2f780a23fbcbe
Nakkasenp65 <nakkasenputtacharoen@gmail.com>
2025-05-02 22:23:01 +0700

    add prediction route

--- app/app.py
+++ app/app.py
@@ -1,8 +1,48 @@
-# Imports
 from flask import Flask, request, jsonify
+import time
+from threading import Lock
 from flask_cors import CORS
+from src.predict import predict_disease
 
 app = Flask(__name__)
 CORS(app)
 
-  
+ip_last_call = {}
+ip_lock = Lock()
+COOLDOWN_SECONDS = 15
+
+
+@app.route("/predict", methods=["GET"])
+def predict_route():
+    user_ip = request.remote_addr
+    current_time = time.time()
+
+    with ip_lock:
+        last_time = ip_last_call.get(user_ip, 0)
+        wait_time = COOLDOWN_SECONDS - (current_time - last_time)
+
+        if wait_time > 0:
+            time.sleep(wait_time)
+
+        ip_last_call[user_ip] = time.time()
+
+    symptom_str = request.args.get("symptoms", "")
+    symptom_list = symptom_str.split(",") if symptom_str else []
+    disease, probability, unknowns = predict_disease(symptom_list)
+
+    return jsonify({
+        "disease": disease,
+        "confidence": round(probability, 4),
+        "unknown_symptoms": unknowns,
+        "symptoms_provided": symptom_list
+    })
+
+@app.route("/")
+def index():
+    return "Welcome to the Disease Prediction API! Use /predict endpoint."
+
+if __name__ == "__main__":
+  app.run( port=5000, debug=True, host="localhost" )
+  print("Server is running on port 5000...")
+
+# GET request for predict, so the user will get the result
--- src/predict.py
+++ src/predict.py
@@ -7,31 +7,38 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 MODEL_PATH = BASE_DIR / "models" / "naive_bayes_model.pkl"
 DATA_PATH = BASE_DIR / "data" / "transformed_dataset_cleaned.csv"
 
+# load model
 model = joblib.load("models/naive_bayes_model.pkl")
 
 df = pd.read_csv(DATA_PATH)
 symptom_columns = df.columns.drop('Disease')
 
 def predict_disease(symptom_list):
+    unknown_list = []
     input_dict = {symptom: False for symptom in symptom_columns}
 
     for symptom in symptom_list:
         if symptom in input_dict:
             input_dict[symptom] = True
         else:
+            unknown_list.append(symptom)
             print(f"⚠️ Warning: '{symptom}' is not a known symptom column.")
 
     
     input_df = pd.DataFrame([input_dict])
 
+    # prediction result
     prediction = model.predict(input_df)[0]
+    
+    # % of prediction
     probability = model.predict_proba(input_df).max()
 
-    return prediction, probability
+    return prediction, probability, unknown_list
 
 # TEST IN MAIN
-if __name__ == "__main__":
-    symptoms = ['shivering', 'chills', 'watering_from_eyes','continuous_sneezing']  # You can change these
-    disease, prob = predict_disease(symptoms)
-    print(f"🩺 Predicted Disease: {disease}")
-    print(f"📊 Confidence: {prob:.2%}")
+# if __name__ == "__main__":
+#     symptoms = ['shivering', 'chills', 'watering_from_eyes','continuous_sneezingssss']  # You can change these
+#     disease, prob, ret_unknown_list = predict_disease(symptoms)
+#     print(f"🩺 Predicted Disease: {disease}")
+#     print(f"📊 Confidence: {prob:.2%}")
+#     print(f" Unknown Symptom: {ret_unknown_list}")
